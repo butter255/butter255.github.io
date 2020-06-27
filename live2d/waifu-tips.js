9fb19011698f63a45d8953a0992bedc68aa1bb09
butter255 <756624613@qq.com>
2020-06-27 21:09:38 +0800

    Site updated: 2020-06-27 21:09:37

--- live2d/waifu-tips.js
+++ live2d/waifu-tips.js
@@ -66,8 +66,8 @@ live2d_settings['waifuDraggableRevert'] = true;         // 松开鼠标还原拖
 // 其他杂项设置
 live2d_settings['l2dVersion']           = '1.4.2';        // 当前版本
 live2d_settings['l2dVerDate']           = '2018.11.12'; // 版本更新日期
-live2d_settings['homePageUrl']          = 'https://removeif.github.io';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
-live2d_settings['aboutPageUrl']         = 'https://removeif.github.io/about/';   // 关于页地址, '{URL 网址}'
+live2d_settings['homePageUrl']          = 'https://butter255.github.io';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
+live2d_settings['aboutPageUrl']         = 'https://butter255.github.io/about/';   // 关于页地址, '{URL 网址}'
 live2d_settings['screenshotCaptureName']= 'live2d.png'; // 看板娘截图文件名，例如 'live2d.png'
 
 /****************************************************************************************************/
